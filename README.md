bb81d0f2e8aa79e020cba29290386df9554e70a5
daniel.derevjanik <daniel.derevjanik@gmail.com>
2017-01-16 15:11:18 +0100

    added `npm install` step to readme

--- README.md
+++ README.md
@@ -1,9 +1,14 @@
 # dnx-exercies
 
+Visit URL: [https://dderevjanik.github.io/dnx-exercise/](https://dderevjanik.github.io/dnx-exercise/)
+
+or
+
 Download and run it on localhost
 
 ```bash
 git clone https://github.com/dderevjanik/dnx-exercise.git
 cd dnx-exercise
+npm install
 npm start
 ```
--- src/redux/appReducer.js
+++ src/redux/appReducer.js
@@ -5,7 +5,7 @@ import { add, remove } from './../utils/immutableHelpers';
 export const appReducer = (state = {}, action) => {
     const rowLength = state.seats[0].length;
     switch(action.type) {
-        case SEAT_TOGGLE:
+        case SEAT_TOGGLE: {
             const { row, col } = action;
             const seatState = state.seats[row][col];
             // if seat is already occupied, do nothing
@@ -16,20 +16,24 @@ export const appReducer = (state = {}, action) => {
             state.seats[row][col] = (1 - seatState);
             // if seat is selected, remove it from selected otherwise add it to selected
             state.selected = seatState
-                ? remove(state.selected, state.selected.indexOf(getPosition(action.row, action.col, rowLength)))
-                : add(state.selected, getPosition(action.row, action.col, rowLength));
+                ? remove(state.selected, state.selected.indexOf(getPosition(row, col, rowLength)))
+                : add(state.selected, getPosition(row, col, rowLength));
             return { ...state }
-        case SEAT_REMOVE:
-            state.seats[action.row][action.col] = 0;
-            state.selected = remove(state.selected, state.selected.indexOf(action.row * state.seats[0].length + action.col));
+        }
+        case SEAT_REMOVE: {
+            const { row, col } = action;
+            state.seats[row][col] = 0;
+            state.selected = remove(state.selected, state.selected.indexOf(row * state.seats[0].length + col));
             return { ...state };
-        case SEAT_REMOVE_ALL:
+        }
+        case SEAT_REMOVE_ALL: {
             state.selected.forEach((seat) => {
                 const {row, col} = getRowCol(seat, rowLength);
                 state.seats[row][col] = 0;
             });
             state.selected = [];
             return { ...state };
+        }
         default:
             return state;
     }
